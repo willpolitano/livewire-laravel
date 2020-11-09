19ccbd33c95c1f91d9781bfd580d229c223aac27
willpolitano <willianpolitano@gmail.com>
2020-11-09 10:56:05 -0300

    login

--- routes/web.php
+++ routes/web.php
@@ -10,4 +10,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/tweets', ShowTweets::class);
+Route::get('/tweets', ShowTweets::class)->middleware('auth');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
